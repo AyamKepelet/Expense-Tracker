3ffd4150647fb0159bedb1051ab21e8199a02e12
AkhmadRafiHaidarrahman <nakanom858@gmail.com>
2026-06-14 23:27:46 +0700

    t

--- src/components/expansecontent/Expanse.jsx
+++ src/components/expansecontent/Expanse.jsx
@@ -1,14 +1,20 @@
 import "./Expanse.css"
 import Content from "./content/Content"
-import { createContext } from "react"
+import MyContext from "/Project Everyday/Expense-Tracker/src/components/context/Mycontext"
+import { useState } from "react"
 export default function Expanse(){
+  const [value,setValue] = useState("")  
+  
+
   return(
     <>
         <div className="w-full justify-center flex h-screen">
           <div className="text-white bg-amber-400 w-max p-8 h-80 rounded-2xl">
             <form className="[&>*]:m-4">
-              <input className="border-2 border-solid" type="text" required placeholder="Expense Name"/>
-              <input className="border-2 border-solid" type="text" required placeholder="Amount"/>
+              <input onChange={(e)=>{
+                setValue(e.target.value)
+              }} className="border-2 border-solid" type="text"  placeholder="Expense Name"/>
+              <input className="border-2 border-solid" type="text"  placeholder="Amount"/>
               <select className="border-2 border-solid">
                 <option value="All">All</option>
                 <option value="Food">Food</option>
@@ -16,15 +22,20 @@ export default function Expanse(){
                 <option value="Entertainment">Entertainment</option>
                 <option value="Others">Others</option>
               </select>
-              <input className="border-2 border-solid" type="date" required/>
-              <button className="bg-green-500 p-1 hover:bg-green-700 transition duration-300">Add Expense</button>
+              <input className="border-2 border-solid" type="date" />
+              <button onClick={(e)=>{
+                e.preventDefault()
+                console.log(value);
+                
+              }} className="bg-green-500 p-1 hover:bg-green-700 transition duration-300">Add Expense</button>
             </form>
             <div>
             </div>
           </div>
         </div>
-
+    <MyContext.Provider>
       <Content/>
+    </MyContext.Provider>
     </>
   )
 }
