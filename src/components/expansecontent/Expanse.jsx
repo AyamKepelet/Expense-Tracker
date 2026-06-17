89444c7352e3321b1b5b8134fb264a77b6c5070f
AkhmadRafiHaidarrahman <nakanom858@gmail.com>
2026-06-17 13:26:06 +0700

    nanti lagi

--- src/components/expansecontent/Expanse.jsx
+++ src/components/expansecontent/Expanse.jsx
@@ -1,20 +1,31 @@
 import "./Expanse.css"
 import Content from "./content/Content"
-import MyContext from "/Project Everyday/Expense-Tracker/src/components/context/Mycontext"
-import { useState } from "react"
+import MyContext from "/Project JS Skilled/Expense Tracker/src/components/context/Mycontext"
+import { useState,useRef } from "react"
 export default function Expanse(){
-  const [value,setValue] = useState("")  
+  const [value,setValue] = useState([])  
+  const [resultName,setResultName] = useState([])
   
+  const inputref = useRef(null)
+  
+  function inputEvent(){
+    inputref.current.value()
+  }
 
+  function takeTheInput(){
+    setResultName(value)
+  }
   return(
     <>
         <div className="w-full justify-center flex h-screen">
           <div className="text-white bg-amber-400 w-max p-8 h-80 rounded-2xl">
             <form className="[&>*]:m-4">
-              <input onChange={(e)=>{
-                setValue(e.target.value)
+              <input ref={inputref} onChange={(e)=>{
+                value.push(e.target.value)
               }} className="border-2 border-solid" type="text"  placeholder="Expense Name"/>
-              <input className="border-2 border-solid" type="text"  placeholder="Amount"/>
+              <input onChange={(e)=>{
+                value.push(e.target.value)
+              }} className="border-2 border-solid" type="text"  placeholder="Amount"/>
               <select className="border-2 border-solid">
                 <option value="All">All</option>
                 <option value="Food">Food</option>
@@ -25,17 +36,20 @@ export default function Expanse(){
               <input className="border-2 border-solid" type="date" />
               <button onClick={(e)=>{
                 e.preventDefault()
+                takeTheInput()
                 console.log(value);
+                console.log(resultName);
+                console.log(inputref);
                 
-              }} className="bg-green-500 p-1 hover:bg-green-700 transition duration-300">Add Expense</button>
+}} className="bg-green-500 p-1 hover:bg-green-700 transition duration-300">Add Expense</button>
             </form>
             <div>
             </div>
-          </div>
-        </div>
-    <MyContext.Provider>
+    <MyContext.Provider value={{resultName}}>
       <Content/>
     </MyContext.Provider>
+          </div>
+        </div>
     </>
   )
 }
--- src/components/expansecontent/content/Content.jsx
+++ src/components/expansecontent/content/Content.jsx
@@ -1,29 +1,42 @@
 import "./content.css"
+import { useContext } from "react"
+import MyContext from "/Project JS Skilled/Expense Tracker/src/components/context/Mycontext"
 
 export default function Content() {
+    const {resultName} = useContext(MyContext) 
     return(
         <>
-        <div className="w-full bg-gray-400">
-            <ul>
-                <li>Expense Name</li>
-                <li></li>
+        <div className="w-full flex justify-center bg-gray-400">
+            <ul className="[&>*]:ml-20 flex">
+                <li className="flex flex-col">
+                    <h3 className="border-t-0 border border-b-0">Expense Name</h3>
+                    <p className="border-t-0 border border-b-0 text-black">{resultName}</p>
+                </li>
+        
+                <li className="flex flex-col">
+                    <h3 className="border-t-0 border border-b-0">Amount</h3>
+                    <p className="border-t-0 border border-b-0 text-black">{resultName}</p>
+                </li>
+            
+        
+                <li className="flex flex-col">
+                    <h3 className="border-t-0 border border-b-0">Category</h3>
+                    <p className="border-t-0 border border-b-0 text-black"></p>
+                </li>
+            
+        
+                <li className="flex flex-col">
+                    <h3 className="border-t-0 border border-b-0">Date</h3>
+                    <p className="border-t-0 border border-b-0 text-black"></p>
+                </li>
+            
+        
+                <li className="flex flex-col">
+                    <h3 className="border-t-0 border border-b-0">Action</h3>
+                    <p className="border-t-0 border border-b-0 text-black"></p>
+                </li>
             </ul>
-            <ul>
-                <li>Amount</li>
-                <li></li>
-            </ul>
-            <ul>
-                <li>Category</li>
-                <li></li>
-            </ul>
-            <ul>
-                <li>Date</li>
-                <li></li>
-            </ul>
-            <ul>
-                <li>Action</li>
-                <li></li>
-            </ul>
+            
         </div>
         </>
     )
